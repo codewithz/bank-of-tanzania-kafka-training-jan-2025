f2de8b7b14fb2605c5281f94226e75cb3963c02a
Zartab Nakhwa <zartab@codewithz.com>
2025-01-07 13:10:50 +0300

    Kafka-Basics

--- kafka-projects/bot-kafka-basics/src/main/java/com/codewithz/ProducerDemo.java
+++ kafka-projects/bot-kafka-basics/src/main/java/com/codewithz/ProducerDemo.java
@@ -1,2 +1,34 @@
-package com.codewithz;public class ProducerDemo {
+package com.codewithz;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+        String bootstrapServers = "localhost:9092";
+        String topic = "bot_first_topic";
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        ProducerRecord<String, String> record = new
+                ProducerRecord<String, String>
+                (topic,"Message from a Java Producer-1");
+
+        producer.send(record);
+
+        producer.flush();
+    }
 }
