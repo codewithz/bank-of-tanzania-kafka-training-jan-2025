ba1ba758dec0b113cd63e2a8daf99bd01496008a
Zartab Nakhwa <zartab@codewithz.com>
2025-01-08 13:19:32 +0300

    Added Validation Logic to Wikimedia Producer

--- kafka-projects/bot-wikimedia-producer/src/main/java/com/codewithz/WikimediaChangesHandler.java
+++ kafka-projects/bot-wikimedia-producer/src/main/java/com/codewithz/WikimediaChangesHandler.java
@@ -1,5 +1,7 @@
 package com.codewithz;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.launchdarkly.eventsource.EventHandler;
 import com.launchdarkly.eventsource.MessageEvent;
 import org.apache.kafka.clients.producer.KafkaProducer;
@@ -31,7 +33,53 @@ public class WikimediaChangesHandler implements EventHandler {
 
     @Override
     public void onMessage(String event, MessageEvent messageEvent) throws Exception {
-//        Send the data to the producer
+
+        boolean isValid=validate(messageEvent);
+        if(isValid){
+            sendRecord(messageEvent);
+        }
+        else{
+            //DO NOTHING
+            logger.info("BOT MESSAGE RECEIVED");
+        }
+
+    }
+
+    @Override
+    public void onComment(String comment) throws Exception {
+
+    }
+
+    @Override
+    public void onError(Throwable t) {
+    logger.error("Error in WikimediaChangesHandler", t);
+    }
+
+    public boolean validate(MessageEvent event){
+        String message=event.getData();
+
+        boolean isValid=false;
+
+        try {
+            // Parse the JSON string
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(message);
+
+            // Check if the "bot" field is true
+            if (rootNode.has("bot") && rootNode.get("bot").asBoolean()) {
+                // Print the JSON if "bot" is true
+               isValid=true;
+            } else {
+                System.out.println("No bot field or bot is not true.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return isValid;
+    }
+
+    public void sendRecord(MessageEvent messageEvent) throws InterruptedException {
+    //       Send the data to the producer
         ProducerRecord<String,String> record = new ProducerRecord<>(topic,messageEvent.getData());
         kafkaProducer.send(record, (metadata, e) -> {
             if (metadata != null) {
@@ -49,16 +97,5 @@ public class WikimediaChangesHandler implements EventHandler {
         });
 
         Thread.sleep(1000);
-
-    }
-
-    @Override
-    public void onComment(String comment) throws Exception {
-
-    }
-
-    @Override
-    public void onError(Throwable t) {
-    logger.error("Error in WikimediaChangesHandler", t);
     }
 }
